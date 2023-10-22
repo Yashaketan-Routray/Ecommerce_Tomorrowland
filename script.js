1d8210632e2d07f9434539a16832d5dd4f56421c
Yashaketan Routray <129492626+Yashaketan-Routray@users.noreply.github.com>
2023-10-22 21:57:27 +0530

    added the js file of objects

---
+++ script.js
@@ -0,0 +1,90 @@
+const mensTShirt = [{
+    "id": 1,
+    "title":" GRAPHICON T-SHIRT",
+    "price": "Rs. 5,400.00",
+    "description": "Feel the energy of the different geometrical shapes, all working together as one to create the Graphicon T-shirt. The remarkable details crafted in combination with the eye-catching Tomorrowland Icon are a reminder that we can create something beautiful when we unite all as one.",
+    "color": ["Green","Black","Pink","Olive","Sand"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+   { "id": 2,
+    "title":" GRAPHICON FRONT T-SHIRT",
+    "price": "Rs. 4,500.00",
+    "description": "Feel the energy of the different geometrical shapes, all working together as one to create the Graphicon T-shirt. The remarkable details crafted in combination with the eye-catching Tomorrowland Icon are a reminder that we can create something beautiful when we unite all as one.",
+    "color": ["Light Grey","Light Blue","Sand"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 3,
+    "title":" LIVE T-SHIRT",
+    "price": "Rs. 5,400.00",
+    "description": "The Live T-shirt urges to celebrate the present moments and to immerse ourselves in the music and experiences that define our lives. It's a call to break free from the confines of routine and truly live in the here and now.",
+    "color": ["Black"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 4,
+    "title":" UNITE T-SHIRT",
+    "price": "Rs. 5,400.00",
+    "description": "The Unite T-shirt is a celebration to create a harmonious symphony of humanity. Unity speaks to the creation of a global family, the People of Tomorrow, made by shared memories and common appreciation for the beauty of human connection.",
+    "color": ["White"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+     "id": 5,
+    "title":" SPRAY ICON T-SHIRT",
+    "price": "Rs. 4,500.00",
+    "description": "The Spray Icon T-shirt takes center stage, a beacon of unity for the People of Tomorrow from every corner of the world. As you slip into its soft embrace, you become a part of Tomorrowland, carrying the magic with you wherever you go. ",
+    "color": ["Black"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 6,
+    "title":" FOUNDATION T-SHIRT",
+    "price": "Rs. 4,500.00",
+    "description": "The Foundation T-shirt is based on drawings created by the children of the Tomorrowland Foundation x Ek Tara Music & Arts School in India. All proceeds of this collection will be donated to the projects of the Tomorrowland Foundation. ",
+    "color": ["Black"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+  
+  {
+   "id": 7,
+    "title":" ICON BLOSSOM T-SHIRT MEN",
+    "price": "Rs. 5,000.00",
+    "description": "Let your flowers blossom roots in your heart with the Icon Blossom T-shirt. The artwork embraces the sense to grow positive vibes across the whole world. Adopt to the pace of nature together with the Tomorrowland Icon adorned with nature's blossoms.",
+    "color": ["Sand"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 8,
+    "title":" GRAPHICON FRONT T-SHIRT",
+    "price": "Rs. 4,500.00",
+    "description": "Feel the energy of the different geometrical shapes, all working together as one to create the Graphicon T-shirt. The remarkable details crafted in combination with the eye-catching Tomorrowland Icon are a reminder that we can create something beautiful when we unite all as one.",
+    "color": ["Sand","Light blue","Light grey"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 9,
+    "title":" TOMORROWLAND T-SHIRT",
+    "price": "Rs. 4,100.00",
+    "description": "The Tomorrowland T-Shirt has a clean look with lettering embroidered on the back. Designed for both style and comfort, this remarkable T-shirt offers a relaxed yet confident fit that ensures freedom of movement. ",
+    "color": ["Dark grey"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  {
+   "id": 10,
+    "title":" LIVE TODAY T-SHIRT",
+    "price": "Rs. 4,500.00",
+    "description": "We are the People of Tomorrow; we feel at home all over the world. The Live Today Sweatshirt embodies the values that define the People of Tomorrow with high-quality Live Today embroidery to give it a clean look for any occasion. The T-shirt has an Oversized fit, which gives it a bold look and guarantees freedom of movement.",
+    "color": ["Black"],
+    "size": ["S","M","L","XL","XXL"]
+  },
+
+  ]
